84b66fdddda2a7680ed99144ff7e4c5a504786a7
luobotang <tangkangxing@sina.com>
2017-03-11 19:42:09 +0800

    move data to app, to share with all components

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ var ItemList = {
     template: '#tpl-item-list',
     data: function () {
         return {
-            items: [{
-                selected: true,
-                name: 'C'
-            }, {
-                selected: true,
-                name: 'Java'
-            }, {
-                selected: true,
-                name: 'C#'
-            }, {
-                selected: false,
-                name: 'JavaScript'
-            }]
+            allSelected: false
         }
     },
+    props: ['items'],
     computed: {
         selectedCount: function () {
             return this.items.filter(item => !!item.selected).length
@@ -26,24 +15,12 @@ var ItemList = {
                 return this.items.findIndex(item => !item.selected) === -1
             },
             set: function (val) {
-                val = !!val;
-                this.items.forEach(item => item.selected = val)
+                val = !!val
+                this.$emit('selectAll', val)
             }
         }
     },
     methods: {
-        addItem: function () {
-            this.items.push({
-                selected: false,
-                name: '[' + (+new Date).toString().slice(5) + ']'
-            })
-        },
-        deleteItem: function (item) {
-            var i = this.items.findIndex(_item => _item === item)
-            if (i !== -1) {
-                this.items.splice(i, 1)
-            }
-        },
         itemDetailUrl: function (name) {
             return '#/item/' + name
         }
@@ -69,7 +46,20 @@ var router = new VueRouter({
 
 var app = new Vue({
     data: {
-        transitionName: 'slide'
+        transitionName: 'slide',
+        items: [{
+            selected: true,
+            name: 'C'
+        }, {
+            selected: true,
+            name: 'Java'
+        }, {
+            selected: true,
+            name: 'C#'
+        }, {
+            selected: false,
+            name: 'JavaScript'
+        }]
     },
     router: router,
     watch: {
@@ -77,6 +67,34 @@ var app = new Vue({
             var toDepth = to.path.split('/').length
             var fromDepth = from.path.split('/').length
             this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+        }
+    },
+    computed: {
+        allSelected: {
+            get: function () {
+                return this.items.findIndex(item => !item.selected) === -1
+            },
+            set: function (val) {
+                val = !!val;
+                this.items.forEach(item => item.selected = val)
+            }
+        }
+    },
+    methods: {
+        addItem: function () {
+            this.items.push(this.newItem())
+        },
+        deleteItem: function (item) {
+            var i = this.items.findIndex(_item => _item === item)
+            if (i !== -1) {
+                this.items.splice(i, 1)
+            }
+        },
+        newItem: function () {
+            return {
+                selected: false,
+                name: (+new Date).toString().slice(5)
+            }
         }
     }
 }).$mount('#app')
